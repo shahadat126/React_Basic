105f154807e56380edd7dc0aba5636da99ae7543
shahadat126 <mdshahadathossainzen@gmail.com>
2026-05-10 15:53:41 +0600

    Immutable convert to mutable

--- src/components/UseRef.jsx
+++ src/components/UseRef.jsx
@@ -2,12 +2,17 @@ import React, { useRef } from 'react'
 
 const UseRef = () => {
     const textRef = useRef(null)
+    const textsRef = useRef(null)
     function handleClick(){
-        textRef.current.innerText = "Text changed"
+         textRef.current.innerText = "Text changed"
+        textsRef.current.innerHTML = "<strong>Html changed ! </strong>"
+        textRef.current.style.color = "red";
+        textRef.current.style.backgroundColor  =  "blue";
     }
   return (
     <div>
         <h1 ref={textRef}>Content Text</h1>
+        <p ref={textsRef}>Html content text</p>
         <button className='border bg-blue-900' onClick={handleClick}>Change Text</button>
     </div>
   )
